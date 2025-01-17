e863f8ab10171ad45bb794c504c16f16ecacbb17
Ingve Moss Liknes <ingve.moss.liknes@appex.no>
2025-01-17 07:50:33 +0100

    Updated app version

--- Helpers/AppVersion.cs
+++ Helpers/AppVersion.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FacebookPanoPrepper.Helpers
+﻿namespace FacebookPanoPrepper.Helpers
 {
     public static class AppVersion
     {
-        public const string Version = "1.1.0";
+        public const string Version = "1.3.0";
         public static string FullVersion => $"v{Version}";
 
         // Semantic version components
         public static readonly int Major = 1;
-        public static readonly int Minor = 1;
+        public static readonly int Minor = 3;
         public static readonly int Patch = 0;
 
         // Build date
-        public static readonly DateTime BuildDate = new DateTime(2025, 1, 16);
+        public static readonly DateTime BuildDate = new DateTime(2025, 1, 17);
     }
 }
